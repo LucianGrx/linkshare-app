8d7568b0ca6059ef0ac4817a8a77292004ea7435
luciangrx <luciangrr@gmail.com>
2024-01-17 20:37:50 +0200

    feat: Organization-tab, Navbar, Sidebar, Organization-selector, Login/Register

---
+++ app/(dashboard)/(panel)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+const OrganizationIdPage = () => {
+  return (
+    <div className="flex flex-col">
+      <p className="text-blue-800 font-bold">Organization Page</p>
+      <p>Minutul la video este de : <span className="text-blue-600">2:44:14</span></p>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: 'My Site App',
+    description: 'Collaborate, manage projects, and reach new productivity peaks.',
+}
